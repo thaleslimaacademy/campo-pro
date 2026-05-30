aaaad6abe7ffbec621359a664c25aae35ebaff6c
Thales Cruz Salviano de Campos <thales@MacBook-Pro-de-Thales.local>
2026-05-30 10:52:06 -0300

    fix: tag a rematriculas

--- app/rematriculas/page.tsx
+++ app/rematriculas/page.tsx
@@ -159,7 +159,8 @@ export default function Rematriculas() {
                   >
                     Rejeitar
                   </button>
-                  {r.whatsappResponsavel && (
+                  {r.whatsappResponsavel                   {r.whatsappResponsavel && (                  {r.whatsappResponsavel && ( (
+                    <a
                     
                       href={"https://wa.me/" + r.whatsappResponsavel.replace(/[^0-9]/g, "")}
                       target="_blank"
