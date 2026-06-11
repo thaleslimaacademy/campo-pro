d5c30c719cbab6f46678132260577a514fbdc980
Thales Cruz Salviano de Campos <thales@MacBook-Pro-de-Thales.local>
2026-06-11 15:14:57 -0300

    fix: next.config limpo com limite server actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActionsBodySizeLimit: '15mb',
+  },
 };
 
 export default nextConfig;
