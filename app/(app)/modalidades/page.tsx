c8f28f2b2753e771c70f9bcb7f40c1faf57cd603
Thales Cruz Salviano de Campos <thales@MacBook-Pro-de-Thales.local>
2026-06-13 09:52:11 -0300

    fix: modalidades force-dynamic

--- app/(app)/modalidades/page.tsx
+++ app/(app)/modalidades/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { useEffect, useState } from 'react'
 import { listarModalidadesEscola, toggleModalidade, getInfoEscola, MODALIDADES, PLANOS_GESTAOFC } from './actions'
 
