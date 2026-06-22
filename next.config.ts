d1d8e10a23171502d24a6357ec42bcbd5a139159
Thales Cruz Salviano de Campos <thales@MacBook-Pro-de-Thales.local>
2026-06-22 10:22:43 -0300

    feat: dominio raiz redireciona para /home

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
+async function redirects() {
+  return [
+    {
+      source: '/',
+      destination: '/home',
+      permanent: false,
+    },
+  ]
+}
+
 const nextConfig: NextConfig = {
+  redirects,
   experimental: {
     serverActions: {
       bodySizeLimit: '15mb',
